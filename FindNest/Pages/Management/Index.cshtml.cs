10114643c9bf06f1a1471faadd517f179f03f1b5
Khac Thanh <thanhcqb2048@gmail.com>
2024-11-04 00:02:01 +0700

    Update management page

---
+++ FindNest/Pages/Management/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FindNest.Pages.Management
+{
+
+    [Authorize]
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
